d2b5babfe8cab8582086e7e81c260eaa61810d53
Heliemdiety <lmj7112004@gmail.com>
2026-03-14 17:44:39 +0530

    Update cpu_transactions.sv

--- dv/env/cpu_transactions.sv
+++ dv/env/cpu_transactions.sv
@@ -1,24 +1,16 @@
-// -----------------------------------------------------------------------------
-// In UVM, we don't think about 1s and 0s. We think about "Objects".
-// This object represents a single instruction moving through your CPU.
-// -----------------------------------------------------------------------------
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
 class cpu_transaction extends uvm_sequence_item;
 
-    // -------------------------------------------------------------------------
     // PHYSICAL DATA (Sampled from the CPU)
-    // -------------------------------------------------------------------------
     logic [31:0] pc;
     logic [31:0] inst;
     logic        rf_we;
     logic [4:0]  rf_waddr;
     logic [31:0] rf_wdata;
 
-    // -------------------------------------------------------------------------
     // UVM FACTORY REGISTRATION
-    // -------------------------------------------------------------------------
     `uvm_object_utils_begin(cpu_transaction)
         `uvm_field_int(pc,       UVM_ALL_ON | UVM_HEX)
         `uvm_field_int(inst,     UVM_ALL_ON | UVM_HEX)
